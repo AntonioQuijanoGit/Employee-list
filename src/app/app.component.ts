062fec18ecb6d78ecb4398687a1dc6aa4d6e0b41
AntonioQuijanoGit <antonioquijanogit@gmail.com>
2024-09-05 13:30:51 +0200

    Add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CommonModule } from '@angular/common';
+import { Employee } from './models/employee';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'employees';
+  employeeArray: Employee[] = [
+    { id: 1, name: 'Ryan', country: 'USA' },
+    { id: 2, name: 'Rick', country: 'USA' },
+    { id: 3, name: 'Daniel', country: 'USA' },
+  ];
+
+  selectedEmployee: Employee = new Employee();
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  delete() {
+    if (confirm('Are you sure you want to delete it?')) {
+      this.employeeArray = this.employeeArray.filter(
+        (x) => x != this.selectedEmployee
+      );
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
